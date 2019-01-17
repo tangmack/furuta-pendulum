6293a9d2275323f8bc034dd8af955ad396b16edb
macktang <macktang@gmail.com>
2019-01-16 21:53:25 -0700

    added timing code, simulation image quality

--- furutasim.py
+++ furutasim.py
@@ -14,6 +14,8 @@ This animation illustrates the Furuta Pendulum problem.
 # using angle position measurements"
 # Authors: Lin Zhao, Shuli Gong, Ancai Zhang, Lanmei Cong
 
+import time
+t0 = time.time()
 
 from numpy import sin, cos
 import numpy as np
@@ -22,17 +24,20 @@ import scipy.integrate as integrate
 import matplotlib.animation as animation
 import cv2
 
+import math
+
+
 G = 9.8  # acceleration due to gravity, in m/s^2
 L1 = 1.0  # length of pendulum 1 in m
 L2 = 1.5  # length of pendulum 2 in m
 
-M1 = 1.0  # mass of pendulum 1 in kg
-R1 = 0.5
-J1 = 1.0
+M1 = 2.5  # mass of pendulum 1 in kg
+R1 = 0.8
+J1 = 3.0
 
-M2 = 2.0  # mass of pendulum 2 in kg
-R2 = 1.0
-J2 = 1.0
+M2 = 1.0  # mass of pendulum 2 in kg
+R2 = 0.75
+J2 = 2.0
 # FRIC1 = .3  # friction coefficient (* by velocity)
 # FRIC2 = .3  # friction coefficient (* by velocity)
 
@@ -40,10 +45,6 @@ A1 = J1 + M1 * R1 * R1
 A2 = M1 * R1 * L2
 A3 = J2 + M2 * R2 * R2 + M1 * L2 * L2
 A4 = M1 * G * R1
-
-# Set up formatting for the movie files
-Writer = animation.writers['ffmpeg']
-writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 
 
 def derivs(state, t):
@@ -53,17 +54,22 @@ def derivs(state, t):
     phi = A1 * state[3] * state[3] * sin(2*state[1]) \
         + A2 * state[3] * (state[1] + A2 * state[3] * cos(state[0]))*sin(state[0])/A1
 
+    
+
     dydx[0] = state[1] + A2 * state[3] * cos(state[0])
     dydx[1] = A4 * sin(state[0]) + phi
 
     dydx[2] = state[3]
     dydx[3] = 0  # Apply a constant torque top motor of 0.1
 
+    print t
     return dydx
 
 # create a time array from 0..100 sampled at 0.01 second steps
 dt = 0.03125
-t = np.arange(0.0, 15, dt)
+t = np.arange(0.0, 7.0, dt)
+print "t is"
+print t
 myfps = int(1/dt)
 myinterval = int(dt*1000)
 
@@ -71,12 +77,12 @@ myinterval = int(dt*1000)
 q1 = 135.0  # angle of pendulum
 q2 = 0.0  # angle of Furuta arm/motor
 q1d = 0.0  # initial angular speed of q1
-q2d = 0.0  # initial angular speed of q2
+q2d = 1.0  # initial angular speed of q2
 
 
 # X1, X2, X3, X4 are the state space representation
 X1 = q1
-X2 = A1 * q1d - A2 * cos(q1 * q2d)
+X2 = A1 * q1d - A2 * cos(math.radians(q1) * math.radians(q2d))
 X3 = q2
 X4 = q2d
 
@@ -87,6 +93,7 @@ state = np.radians([X1, X2, X3, X4])
 y = integrate.odeint(derivs, state, t)
 
 x1 = -L1*sin(y[:, 0])
+print y[:,0]
 y1 = L1*cos(y[:, 0])
 
 x2 = L2*sin(y[:, 2])
@@ -94,6 +101,7 @@ y2 = -L2*cos(y[:, 2])
 
 fig = plt.figure()
 ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
+fig.gca().set_aspect('equal',adjustable='box')
 ax.grid()
 
 line, = ax.plot([], [], 'o-', lw=2)
@@ -107,7 +115,6 @@ def init():
     time_text.set_text('')
     return line, time_text
 
-
 def animate(i):
     thisx = [0, x1[i]]
     thisy = [0, y1[i]]
@@ -120,8 +127,16 @@ def animate(i):
     time_text.set_text(time_template % (i*dt))
     return line, line2, time_text
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, len(y)),
-                              interval=32, blit=True, init_func=init)
-# ani.save('f3.mp4', fps=myfps, writer='ffmpeg')
-plt.show()
 
+ani = animation.FuncAnimation(fig, animate, np.arange(1, len(y)),
+                              interval=0, blit=False, init_func=init)
+
+# Set up formatting for the movie files
+# Writer = animation.writers['ffmpeg']
+# writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+
+ani.save('f3.mp4', fps=myfps, dpi=60, writer='ffmpeg')
+# plt.show()
+
+t1 = time.time()
+print(t1-t0)
